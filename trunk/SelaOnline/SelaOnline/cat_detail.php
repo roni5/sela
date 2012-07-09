0ac5521a62057f5af4b857af48634d5c75306768
lethanhviet88 <lethanhviet88@ef27217b-3a03-387d-1fd9-fd887424bbfe>
2012-07-09 17:50:03 +0000

    update

--- trunk/SelaOnline/SelaOnline/cat_detail.php
+++ trunk/SelaOnline/SelaOnline/cat_detail.php
@@ -3,8 +3,10 @@
 /* TODO: Add code here */
 require('config/globalconfig.php');
 include_once('class/model_articletype.php');
+include_once('class/model_article.php');
 
 $objArticleType = new model_ArticleType($objConnection);
+$objArticle = new model_Article($objConnection);
 $htmlCategory =  $objArticleType->DisplayAllCategory();
 
 ?>
@@ -12,48 +14,17 @@ $htmlCategory =  $objArticleType->DisplayAllCategory();
 <?php
 include_once('include/_header.inc');
 include_once('include/_menu.inc');
+if ($_pgR["catid"])
+{
+	$catID = $_pgR["catid"];
+	$condition = global_mapping::ArticleType. '=\''.$catID.'\'';
+	$orderBy = global_mapping::CreatedDate. ' DESC ';
+	$arrArticle = $objArticle->getAllArticle('*',$condition,$orderBy);
+}
+include_once('include/_article_list.inc');
 ?>
 
-<div class="article-memo">
-	<div class="article-memo-detail">
-		<div class="article-memo-content">
-			<div class="article-memo-control">control</div>
-			<h2>HELLO247-CHUYÊN BÁN BA LÔ, TÚI XÁCH ĐI LÀM-ĐỰNG LAPTOP-ĐI HỌC- NAM NỮ GIÁ hấp dẫn</h2>
-			<div class="article-short-info">
-				<span><b>sukoi</b> Comment: 24-12-2012</span>
-				<span class="paging"> 
-					<a>1</a> <a> 2</a 
-				</span>
-			</div>
-		</div>
-	</div>
-	<div class="article-memo-user">
-		<div class="user-info">
-			<div><img src="/image/default/default_logo.jpg" width="120" height="100" /></div>
-			User info
-		</div>
-	</div>
-</div>
-<div class="article-memo">
-	<div class="article-memo-detail">
-		<div class="article-memo-content">
-			<div class="article-memo-control">control</div>
-			<h2>HELLO247-CHUYÊN BÁN BA LÔ, TÚI XÁCH ĐI LÀM-ĐỰNG LAPTOP-ĐI HỌC- NAM NỮ GIÁ hấp dẫn</h2>
-			<div class="article-short-info">
-				<span><b>sukoi</b> Comment: 24-12-2012</span>
-				<span class="paging"> 
-					<a>1</a> <a> 2</a 
-				</span>
-			</div>
-		</div>
-	</div>
-	<div class="article-memo-user">
-		<div class="user-info">
-			<div><img src="/image/default/default_logo.jpg" width="120" height="100" /></div>
-			User info
-		</div>
-	</div>
-</div>
+
 <?php 
 //footer
 include_once('include/_footer.inc');
